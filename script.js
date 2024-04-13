cb4ef743ee56dafc9fc9f5e3f07fae0b7d89e6a9
anand dhar dwivedi <ananddhardwivedi05@gmail.com>
2024-04-13 10:29:33 +0530

    13-04-2024

---
+++ script.js
@@ -0,0 +1,50 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function deleteAllTask() {
+  if (confirm("Are you sure you want to delete all tasks?")) {
+    listContainer.innerHTML = ""; // Clear the list container
+    saveData(); // Save the updated data to localStorage
+  }
+}
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("its empty ❌");
+  } else {
+    let ele = document.createElement("li");
+    ele.setAttribute("class", "taskDisplay");
+    ele.innerHTML = inputBox.value;
+    listContainer.appendChild(ele);
+    let button = document.createElement("button");
+    button.setAttribute("class", "myButtonClass");
+    button.innerText = "Delete";
+    ele.appendChild(button);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "BUTTON") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function setData() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+setData();
