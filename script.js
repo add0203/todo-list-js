3b715ee421b951aad1f461117ebd972acb0bf2f8
Anand Dhar Dwivedi <120220077+add0203@users.noreply.github.com>
2024-04-14 22:47:58 +0530

    script.js

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function addTask() {
     listContainer.appendChild(ele);
     let button = document.createElement("button");
     button.setAttribute("class", "myButtonClass");
+    document.getElementByClassName("myButtonClass").style.backgroundColor = "red";
     button.innerText = "Delete";
     ele.appendChild(button);
   }
